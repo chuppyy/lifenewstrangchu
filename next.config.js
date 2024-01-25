52ecbb293102bfdecbd6c386237d48f6f58916a2
chuppyy <takhanhthient3@GMAIL.COM>
2024-01-25 20:35:38 +0700

    l

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "apisport.sportsandtravelonline.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
